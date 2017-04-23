f7599ef55f79f814e1be9327ccef1293b1867a81
Julia Chen Jing <juliachenjing@gmail.com>
2017-04-23 12:56:36 -0500

    Update LogAnalyser.scala

--- src/main/java/crystal/spark/LogAnalyser.scala
+++ src/main/java/crystal/spark/LogAnalyser.scala
@@ -1,6 +1,3 @@
-/**
-  * Created by cloudera on 10/22/16.
-  */
 package crystal.spark
 
 import java.util.regex.Pattern
@@ -14,14 +11,13 @@ import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.SparkContext
 
 
-object LogAnalyser extends Serializable{
-  def main(args: Array[String]) {
-
-
+object LogAnalyser extends Serializable
+{
+  def main(args: Array[String]) 
+ {
     val sc = new SparkContext()
 
     case class Record(method: String, url: String, code: String)
-
 
     val input = sc.textFile("input", 5).cache()
     def parseLine(line:String):Option[Record] = {
@@ -32,8 +28,6 @@ object LogAnalyser extends Serializable{
         None
       }
     }
-
-
 
     val results = input.flatMap(line=>parseLine(line))
                         .filter(line => !(line.code.equals("") || line.url.equals("") || line.method.equals("")))
@@ -50,7 +44,6 @@ object LogAnalyser extends Serializable{
                         })
 
     val resultsForTextOutput = results.sortBy(f=>(f._1._1, f._1._2)).map(x=>x._1._2 + "\t" + x._2 + "\t" + x._1._1)
-
 
 
     val conf = HBaseConfiguration.create()
